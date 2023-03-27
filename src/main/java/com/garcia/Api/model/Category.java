36a458212cb5ee350cfa297406413738cdce76fa
Alex García Álvarez <alexgarciaalvarez93@gmail.com>
2023-03-27 20:46:00 +0200

    Revert "only courses"
    This reverts commit a1bc5f742369186407325432f3c3d922669a1144.

---
+++ src/main/java/com/garcia/Api/model/Category.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.garcia.Api.model;
+
+import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ *
+ * @author alexgaralv
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String category;
+}
